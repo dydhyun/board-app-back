33870d0f0c695d145c959de3860b233254ec569c
dydhyun <dydgus625@naver.com>
2024-09-27 10:45:02 +0900

    board-app-backend

---
+++ src/main/java/com/bit/boardappbackend/config/WebMvcConfiguration.java
@@ -0,0 +1,18 @@
+package com.bit.boardappbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://110.165.16.121:3000")//front-server 공인 Ip
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
